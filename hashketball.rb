1b702779d6089789e46063b69d0fc48e9f6ad50d
Islam Madkaur <islammadkaur@gmail.com>
2020-10-25 01:01:19 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -128,3 +128,235 @@ def game_hash
 end
 
 # Write code here
+# Write your code below game_hash
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        {
+          player_name: "Reggie Evans",
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+        {
+          player_name: "Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+        {
+          player_name: "Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 11,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        {
+          player_name: "Jason Terry",
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      ]
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: [
+        {
+          player_name: "Jeff Adrien",
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        },
+        {
+          player_name: "Bismack Biyombo",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 22,
+          blocks: 15,
+          slam_dunks: 10
+        },
+        {
+          player_name: "DeSagna Diop",
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        },
+        {
+          player_name: "Ben Gordon",
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        },
+        {
+          player_name: "Kemba Walker",
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 7,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      ]
+    }
+  }
+end
+
+# Write code here
+def num_points_scored(player)
+
+  game_hash.each do |key_home, value|
+    value[:players].each do |player_element|
+
+      if player_element[:player_name] == player
+        return player_element[:points]
+      end
+    end
+  end
+end
+
+
+def shoe_size(player)
+  game_hash.each do |key_home, value|
+    value[:players].each do |player_element|
+
+      if player_element[:player_name] == player
+        return player_element[:shoe]
+      end
+    end
+  end
+end
+
+
+def team_colors(team_name)
+    game_hash.each do |key_home, value|
+
+      if value[:team_name] == team_name
+        return value[:colors]
+      end
+  end
+end
+
+
+def team_names
+  team_n_array = []
+
+  game_hash.each do |key_home, value|
+    value.find do
+
+      if value[:team_name]
+        team_n_array << value[:team_name]
+      end
+    end
+  end
+    team_n_array
+end
+
+
+def player_numbers(team_name)
+    jersey_n_array = []
+
+  game_hash.each do |key_home, value|
+    value.find do
+
+      if value[:team_name] == team_name
+        player = value[:players]
+        player.each do |element|
+          jersey_n_array << element[:number]
+        end
+      end
+    end
+  end
+    jersey_n_array
+end
+
+
+def player_stats(name)
+      player_stats = {}
+
+  game_hash.each do |key_home, value|
+    value.find do
+      value[:players].each do |element|
+
+          if element[:player_name] == name
+           player_stats = element
+       end
+      end
+    end
+  end
+  player_stats
+end
+
+
+def big_shoe_rebounds
+  shoe_rebounds = []
+  game_hash.each do |key_home, value|
+    value.find do
+      value[:players].each do |element|
+
+         element.find do |shoe|
+           shoe_rebounds<<{element[:shoe] => element[:rebounds]}
+          end
+
+        end
+      end
+    end
+  big_shoe = shoe_rebounds.max_by(&:first)
+  big_shoe.each_value do |rebound|
+    return rebound
+  end
+end
